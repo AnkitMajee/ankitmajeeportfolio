c40d914397d07812fc438bcec216d49d7560559e
Ankit Majee <ankitmajee.05@gmail.com>
2024-08-21 20:40:57 +0530

    Added Grid as Background

---
+++ components/Grid.tsx
@@ -0,0 +1,18 @@
+import { BentoGrid, BentoGridItem } from './ui/BentoGrid'  
+const Grid = () => { 
+return (
+< section id = "about" > 
+<BentoGrid> 
+    {[{ title: 'Title', description: 'Desc1', id: "1"
+     }].map((item) => (
+     <BentoGridItem 
+     id={item.id} 
+     key={item.id} 
+     title={item.title} 
+     description={item.description}
+    />
+    ))} 
+ </BentoGrid> 
+ </section >
+    )
+    }
