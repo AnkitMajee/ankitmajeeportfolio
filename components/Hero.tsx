85fe603b6994b1571006d30f84487af56c8eb364
Ankit Majee <ankitmajee.05@gmail.com>
2024-08-24 19:17:33 +0530

    Added recent Projects

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,4 @@
-import MagicButton from './ui/MagicButton'
+import MagicButton from './MagicButton'
 import {Spotlight} from './ui/Spotlight'
 import { TextGenerateEffect } from './ui/text-generate-effect'
 import { FaLocationArrow } from "react-icons/fa6";
@@ -11,9 +11,13 @@ const Hero=()=> {
         <Spotlight className="-top-40 left-full h-[80vh] w-[50vw]" fill="purple"/>
         <Spotlight className="-top-40 -left-10 h-[80vh] w-[50vw]" fill="blue"/>
       </div>
-      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.05] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
+      <div className="h-screen w-full dark:bg-black-100 bg-white  
+      dark:bg-grid-white/[0.05] bg-grid-black/[0.2] flex items-center 
+      justify-center absolute top-0 left-0">
       {/* Radial gradient for the container to give a faded look */}
-      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"/>
+      <div className="absolute pointer-events-none inset-0 
+      flex items-center justify-center dark:bg-black-100
+       bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"/>
       </div>
       <div className='flex justify-center relative my-20 z-10'>
         <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
