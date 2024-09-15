64e7b588de1c7440db88abd34006dac4d65cb1b2
Ankit Majee <ankitmajee.05@gmail.com>
2024-09-15 23:19:03 +0530

    About Us changes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -57,7 +57,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Ankit, a Full stack developer Developer based in Croatia.
+            Hi! I&apos;m Ankit, a Full stack developer Developer based in Kolkata.
           </p>
 
           <a href="#about">
