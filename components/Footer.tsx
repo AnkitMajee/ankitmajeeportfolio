58d658d696391bd948c1935a70c57cd4a5ce72c4
Ankit Majee <ankitmajee.05@gmail.com>
2024-09-16 21:10:09 +0530

    Footer link Update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -39,12 +39,21 @@ const Footer = () => {
 
         <div className="flex items-center md:gap-3 gap-6">
           {socialMedia.map((info) => (
-            <div
-              key={info.id}
-              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
-            >
-              <img src={info.img} alt="icons" width={20} height={20} />
-            </div>
+            // <div
+            //   key={info.id}
+            //   className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+            // >
+            //   <img src={info.img} alt="icons" width={20} height={20} />
+            // </div>
+            <a
+      key={info.id}
+      href={info.link}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="w-10 h-10 cursor-pointer flex justify-center items-center rounded-lg bg-opacity-75 backdrop-filter backdrop-blur-lg saturate-180 border border-black-300"
+    >
+      <img src={info.img} alt={info.img.split('.')[0]} width={20} height={20} />
+    </a>
           ))}
         </div>
       </div>
--- data/index.ts
+++ data/index.ts
@@ -174,45 +174,48 @@ export const navItems = [
   export const workExperience = [
     {
       id: 1,
-      title: "Frontend Engineer Intern",
-      desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+      title: "Software Developer Intern - Bluestock Fintech",
+      desc: "Assisted in the development of a web-based platform using HTML,CSS,Python,RestAPI,testing enhancing interactivity.",
       className: "md:col-span-2",
       thumbnail: "/exp1.svg",
     },
     {
       id: 2,
-      title: "Mobile App Dev - JSM Tech",
-      desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+      title: "Product Designer Intern - Zummit labs",
+      desc: "Designed UI/UX design trends, principles, and methodologies across mobile and web.",
       className: "md:col-span-2", // change to md:col-span-2
       thumbnail: "/exp2.svg",
     },
-    {
-      id: 3,
-      title: "Freelance App Dev Project",
-      desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
-      className: "md:col-span-2", // change to md:col-span-2
-      thumbnail: "/exp3.svg",
-    },
-    {
-      id: 4,
-      title: "Lead Frontend Developer",
-      desc: "Developed and maintained user-facing features using modern frontend technologies.",
-      className: "md:col-span-2",
-      thumbnail: "/exp4.svg",
-    },
+    // {
+    //   id: 3,
+    //   title: "Freelance App Dev Project",
+    //   desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    //   className: "md:col-span-2", // change to md:col-span-2
+    //   thumbnail: "/exp3.svg",
+    // },
+    // {
+    //   id: 4,
+    //   title: "Lead Frontend Developer",
+    //   desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    //   className: "md:col-span-2",
+    //   thumbnail: "/exp4.svg",
+    // },
   ];
   
   export const socialMedia = [
     {
       id: 1,
       img: "/git.svg",
+      link:"https://github.com/AnkitMajee",
     },
     {
       id: 2,
       img: "/twit.svg",
+      link:"https://twitter.com/Ankit_Majee",
     },
     {
       id: 3,
       img: "/link.svg",
+      link:"https://www.linkedin.com/in/ankitmajee",
     },
   ];
